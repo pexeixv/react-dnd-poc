d8c1885c7da6e80899af03009f1b689f7f85aab7
pexeixv <gavineditz@gmail.com>
2024-07-05 10:35:22 +0530

    Task creation and deletion

--- src/components/ReactMovableDayWiseTasks.jsx
+++ src/components/ReactMovableDayWiseTasks.jsx
@@ -36,37 +36,28 @@ export default function ReactMovable() {
   const addDay = () => {
     setDays((prevDays) => [
       ...prevDays,
-      { name: `Day ${prevDays.length + 1}`, tasks: [] },
+      { name: `Day ${prevDays.length + 1}`, tasks: [], newTask: "" },
     ]);
   };
 
-  const addTask = (dayIndex) => {
-    setDays((prevDays) =>
-      prevDays.map((day, index) => {
-        if (index === dayIndex) {
-          return {
-            ...day,
-            tasks: [...day.tasks, `Task ${taskCounter}`],
-          };
-        }
-        return day;
-      })
-    );
-    setTaskCounter(taskCounter + 1);
-  };
-
-  const resetTasks = (dayIndex) => {
-    setDays((prevDays) =>
-      prevDays.map((day, index) => {
-        if (index === dayIndex) {
-          return {
-            ...day,
-            tasks: [],
-          };
-        }
-        return day;
-      })
-    );
+  const addTask = (e, dayIndex) => {
+    e.preventDefault();
+    const newTask = days[dayIndex].newTask.trim();
+    if (newTask) {
+      setDays((prevDays) =>
+        prevDays.map((day, index) => {
+          if (index === dayIndex) {
+            return {
+              ...day,
+              tasks: [...day.tasks, newTask],
+              newTask: "",
+            };
+          }
+          return day;
+        })
+      );
+      setTaskCounter(taskCounter + 1);
+    }
   };
 
   const handleTaskReorder = (dayIndex, { oldIndex, newIndex }) => {
@@ -83,46 +74,101 @@ export default function ReactMovable() {
     );
   };
 
+  const resetTasks = (dayIndex) => {
+    setDays((prevDays) =>
+      prevDays.map((day, index) => {
+        if (index === dayIndex) {
+          return {
+            ...day,
+            tasks: [],
+          };
+        }
+        return day;
+      })
+    );
+  };
+
+  const handleNewTaskChange = (dayIndex, event) => {
+    const value = event.target.value;
+    setDays((prevDays) =>
+      prevDays.map((day, index) => {
+        if (index === dayIndex) {
+          return {
+            ...day,
+            newTask: value,
+          };
+        }
+        return day;
+      })
+    );
+  };
+
+  const deleteTask = (dayIndex, taskIndex) => {
+    setDays((prevDays) =>
+      prevDays.map((day, index) => {
+        if (index === dayIndex) {
+          return {
+            ...day,
+            tasks: day.tasks.filter((_, idx) => idx !== taskIndex),
+          };
+        }
+        return day;
+      })
+    );
+  };
+
   return (
     <section>
-      <div className="container mx-auto px-5 py-10">
+      <div className="container px-5 py-10 mx-auto">
         <h2 className="text-3xl font-bold">Day wise Tasks (react-movable)</h2>
-        <div className="mt-4 flex gap-4">
+        <div className="flex gap-4 mt-4">
           <button
             onClick={addDay}
-            className="bg-slate-900 text-white cursor-pointer list-none p-2 text-center text-xs"
+            className="p-2 text-xs text-center text-white list-none cursor-pointer bg-slate-900 whitespace-nowrap"
           >
-            <i className="fas fa-plus mr-1"></i>
+            <i className="mr-1 fas fa-plus"></i>
             Add Day
           </button>
           <button
             onClick={resetData}
-            className="bg-red-500 text-white cursor-pointer list-none p-2 text-center text-xs"
+            className="p-2 text-xs text-center text-white list-none bg-red-500 cursor-pointer whitespace-nowrap"
           >
-            <i className="fas fa-trash mr-1"></i>
+            <i className="mr-1 fas fa-trash"></i>
             Reset Days
           </button>
         </div>
-        <div className="grid gap-8 grid-cols-2 md:grid-cols-4 mt-8">
+        <div className="grid grid-cols-2 gap-8 mt-8 lg:grid-cols-4">
           {days.map((day, dayIndex) => (
             <div key={dayIndex} className="mb-10">
-              <h3 className="text-2xl font-bold">{day.name}</h3>
-              <div className="mt-4 flex gap-4">
-                <button
-                  onClick={() => addTask(dayIndex)}
-                  className="bg-slate-900 text-white cursor-pointer list-none p-2 text-center text-xs"
-                >
-                  <i className="fas fa-plus mr-1"></i>
-                  Add Task
-                </button>
+              <div className="flex justify-between gap-4 mt-4">
+                <h3 className="text-2xl font-bold">{day.name}</h3>
                 <button
                   onClick={() => resetTasks(dayIndex)}
-                  className="bg-red-500 text-white cursor-pointer list-none p-2 text-center text-xs"
+                  className="p-2 text-xs text-center text-white list-none bg-red-500 cursor-pointer whitespace-nowrap"
                 >
-                  <i className="fas fa-trash mr-1"></i>
+                  <i className="mr-1 fas fa-trash"></i>
                   Reset Tasks
                 </button>
               </div>
+
+              <form
+                onSubmit={(e) => addTask(e, dayIndex)}
+                className="flex justify-between gap-4 mt-8"
+              >
+                <input
+                  type="text"
+                  placeholder="Add new task"
+                  className="flex-1 h-10 px-2 border"
+                  value={day.newTask}
+                  maxLength={50}
+                  minLength={1}
+                  onChange={(e) => handleNewTaskChange(dayIndex, e)}
+                />
+                <button className="grid h-10 text-white list-none cursor-pointer bg-slate-900 place-items-center aspect-square">
+                  <i className="fas fa-plus"></i>
+                </button>
+              </form>
+
               <List
                 values={day.tasks}
                 onChange={({ oldIndex, newIndex }) =>
@@ -130,19 +176,24 @@ export default function ReactMovable() {
                 }
                 renderList={({ children, props }) => (
                   <ul
-                    className="mt-4 flex flex-col gap-2 w-full max-w-sm"
+                    className="flex flex-col w-full max-w-sm gap-2 mt-4"
                     {...props}
                   >
                     {children}
                   </ul>
                 )}
-                renderItem={({ value, props }) => (
-                  <li
-                    className="border border-slate-900 cursor-pointer list-none p-2 text-center bg-white"
-                    {...props}
-                  >
-                    {value}
-                  </li>
+                renderItem={({ value, props, index }) => (
+                  <div className="flex gap-4" {...props}>
+                    <li className="flex-1 p-2 text-center list-none bg-white border cursor-pointer border-slate-900">
+                      {value}
+                    </li>
+                    <button
+                      onClick={() => deleteTask(dayIndex, index)}
+                      className="h-10 p-2 text-xs text-center text-white list-none bg-red-500 cursor-pointer aspect-square"
+                    >
+                      <i className="fas fa-trash"></i>
+                    </button>
+                  </div>
                 )}
               />
             </div>
